623009b80df1cb9984e2b5b1f5d3aae387ffe90c
Calvin Yoh <flashfire340@gmail.com>
2020-05-10 17:31:06 -0700

    Update cyoh001_lab7_part2.c

--- turnin/cyoh001_lab7_part2.c
+++ turnin/cyoh001_lab7_part2.c
@@ -12,20 +12,26 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "timer.h"
+#include "io.h"
 #endif
 
 void Tick();
 enum States { Start, FIRST, SECOND, THIRD, SECONDSECOND, STOP, RESTART } state;
 unsigned char tempA = 0x00;
 unsigned char tempB = 0x00;
+unsigned char currScore = 0x05;
 
 int main(void)
 {
 	DDRA = 0x00;	PORTA = 0xFF;
 	DDRB = 0xFF;	PORTB = 0x00;
+	DDRC = 0xFF;	PORTC = 0x00;
+	DDRD = 0xFF;	PORTD = 0x00;
 	state = Start;
 	TimerSet(300);
 	TimerOn();
+	LCD_init();
+	LCD_WriteData(currScore + '0');
 	while (1) {
 		Tick();
 		while (!TimerFlag);
@@ -149,8 +155,46 @@ void Tick()
 		tempB = 0x02;
 		break;
 	}
+	case STOP:
+	{
+		if (tempB == 0x02)
+		{
+			if (currScore >= 0x09)
+			{
+				currScore = 0x09;
+				break;
+			}
+			else
+			{
+				currScore = currScore + 0x01;
+				break;
+			}
+		}
+		else
+		{
+			if (currScore <= 0x00)
+			{
+				currScore = 0x00;
+				break;
+			}
+			else
+			{
+				currScore = currScore - 0x01;
+				break;
+			}
+		}
+	}
 	default:
 		break;
 	}
 	PORTB = tempB;
+	LCD_Cursor(1);
+	if (currScore == 0x09)
+	{
+		LCD_DisplayString(1, "WINNER!!");
+	}
+	else
+	{
+		LCD_WriteData(currScore + '0');
+	}
 }
