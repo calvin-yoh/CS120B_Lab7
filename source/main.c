0f2ded85edbbbea435baa5a1a764ee732752c5c8
Calvin Yoh <flashfire340@gmail.com>
2020-05-10 17:03:30 -0700

    update

--- source/main.c
+++ source/main.c
@@ -1,180 +1,175 @@
-/*	Author: cyoh001
+/*	Author: lab
  *  Partner(s) Name:
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
- *
+ *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>	
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "timer.h"
 #include "io.h"
-#endif	
+#endif
 
-enum States { Start, Begin, Increase, Decrease, WaitInc, WaitDec, Reset }state;
-unsigned char tempB = 0x00;
+void Tick();
+enum States { Start, FIRST, SECOND, THIRD, SECONDSECOND, STOP, RESTART } state;
 unsigned char tempA = 0x00;
-int increaseTimer = 0;
-int decreaseTimer = 0;
-
-void Tick() {
-	tempA = ~PINA;
-	switch (state) { //Transitions
-	case Start:
-	{
-		state = Begin;
-		break;
-	}
-	case Begin:
-		if (tempA == 0x01)
-		{
-			state = Increase;
-			break;
-		}
-		else if (tempA == 0x02)
-		{
-			state = Decrease;
-			break;
-		}
-		else if (tempA == 0x03)
-		{
-			state = Reset;
-			break;
-		}
-		else
-		{
-			state = Begin;
-			break;
-		}
-	case Increase:
-		increaseTimer = 0;
-		state = WaitInc;
-		break;
-	case Decrease:
-		decreaseTimer = 0;
-		state = WaitDec;
-		break;
-	case WaitInc:
-		if (increaseTimer >= 10)
-		{
-			state = Increase;
-			break;
-		}
-		else if (tempA == 0x01)
-		{
-			increaseTimer++;
-			state = WaitInc;
-			break;
-		}
-		else if (tempA == 0x03)
-		{
-			state = Reset;
-			break;
-		}
-		else
-		{
-			state = Begin;
-			break;
-		}
-	case WaitDec:
-		if (decreaseTimer >= 10)
-		{
-			state = Decrease;
-			break;
-		}
-		else if (tempA == 0x02)
-		{
-			decreaseTimer++;
-			state = WaitDec;
-			break;
-		}
-		else if (tempA == 0x03)
-		{
-			state = Reset;
-			break;
-		}
-		else
-		{
-			state = Begin;
-			break;
-		}
-	case Reset:
-		if ((tempA == 0x01) || (tempA == 0x02))
-		{
-			state = Reset; break;
-		}
-		else
-		{
-			state = Begin; break;
-		}
-	default:
-		state = Start;
-		break;
-	}
-
-	switch (state) { //State actions
-	case Begin:
-		break;
-	case Increase:
-	{
-		if (tempB >= 0x09)
-		{
-			tempB = 0x09;
-			break;
-		}
-		else
-		{
-			tempB = tempB + 0x01;
-			break;
-		}
-	}
-	case Decrease:
-	{
-		if (tempB <= 0x00)
-		{
-			tempB = 0x00;
-			break;
-		}
-		else
-		{
-			tempB = tempB - 0x01;
-			break;
-		}
-	}
-	case Reset:
-	{
-		tempB = 0x00;
-		break;
-	}
-	default:
-		break;
-	}
-
-	LCD_Cursor(1);
-	LCD_WriteData(tempB + '0');
-}
+unsigned char tempB = 0x00;
+unsigned char currScore = 0x05;
 
 int main(void)
 {
-	state = Start;
-	tempB = 0x00;
 	DDRA = 0x00;	PORTA = 0xFF;
+	DDRB = 0xFF;	PORTB = 0x00;
 	DDRC = 0xFF;	PORTC = 0x00;
 	DDRD = 0xFF;	PORTD = 0x00;
-	TimerSet(100);
+	state = Start;
+	TimerSet(300);
 	TimerOn();
 	LCD_init();
-	LCD_WriteData(tempB + '0');
-	while (1)
-	{
+	LCD_WriteData(currScore + '0');
+	while (1) {
 		Tick();
 		while (!TimerFlag);
 		TimerFlag = 0;
 	}
-	return 0;
+}
+
+void Tick()
+{
+	tempA = ~PINA;
+	switch (state) //Transitions
+	{
+	case Start:
+	{
+		tempB = 0x00;
+		state = FIRST;
+		break;
+	}
+	case FIRST:
+	{
+		if (tempA == 0x01)
+		{
+			state = STOP;
+			break;
+		}
+		else
+		{
+			state = SECOND;
+			break;
+		}
+	}
+	case SECOND:
+	{
+		if (tempA == 0x01)
+		{
+			state = STOP;
+			break;
+		}
+		else
+		{
+			state = THIRD;
+			break;
+		}
+	}
+	case THIRD:
+	{
+		if (tempA == 0x01)
+		{
+			state = STOP;
+			break;
+		}
+		else
+		{
+			state = SECONDSECOND;
+			break;
+		}
+	}
+	case SECONDSECOND:
+	{
+		if (tempA == 0x01)
+		{
+			state = STOP;
+			break;
+		}
+		else
+		{
+			state = FIRST;
+			break;
+		}
+	}
+	case STOP:
+	{
+		if (tempA == 0x01)
+		{
+			state = STOP;
+			break;
+		}
+		else
+		{
+			state = RESTART;
+			break;
+		}
+	}
+	case RESTART:
+	{
+		if (tempA == 0x01)
+		{
+			state = FIRST;
+			break;
+		}
+		else
+		{
+			state = RESTART;
+			break;
+		}
+
+	}
+	default:
+		break;
+	}
+
+	switch (state) //State Actions
+	{
+	case FIRST:
+	{
+		tempB = 0x01;
+		break;
+	}
+	case SECOND:
+	{
+		tempB = 0x02;
+		break;
+	}
+	case THIRD:
+	{
+		tempB = 0x04;
+		break;
+	}
+	case SECONDSECOND:
+	{
+		tempB = 0x02;
+		break;
+	}
+	case STOP:
+	{
+		if (tempB == 0x02)
+		{
+			currScore++;
+		}
+		else
+		{
+			currScore--;
+		}
+	}
+	default:
+		break;
+	}
+	PORTB = tempB;
+	LCD_Cursor(1);
+	LCD_WriteData(tempB + '0');
 }
--- turnin/cyoh001_lab7_part1.c
+++ turnin/cyoh001_lab7_part1.c
@@ -1,81 +1,180 @@
-/*	Author: lab
+/*	Author: cyoh001
  *  Partner(s) Name:
  *	Lab Section:
  *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+ *
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <stdlib.h>	
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #include "timer.h"
-#endif
+#include "io.h"
+#endif	
 
-void Tick();
-enum States { START, FIRST, SECOND, THIRD }state;
+enum States { Start, Begin, Increase, Decrease, WaitInc, WaitDec, Reset }state;
 unsigned char tempB = 0x00;
+unsigned char tempA = 0x00;
+int increaseTimer = 0;
+int decreaseTimer = 0;
+
+void Tick() {
+	tempA = ~PINA;
+	switch (state) { //Transitions
+	case Start:
+	{
+		state = Begin;
+		break;
+	}
+	case Begin:
+		if (tempA == 0x01)
+		{
+			state = Increase;
+			break;
+		}
+		else if (tempA == 0x02)
+		{
+			state = Decrease;
+			break;
+		}
+		else if (tempA == 0x03)
+		{
+			state = Reset;
+			break;
+		}
+		else
+		{
+			state = Begin;
+			break;
+		}
+	case Increase:
+		increaseTimer = 0;
+		state = WaitInc;
+		break;
+	case Decrease:
+		decreaseTimer = 0;
+		state = WaitDec;
+		break;
+	case WaitInc:
+		if (increaseTimer >= 10)
+		{
+			state = Increase;
+			break;
+		}
+		else if (tempA == 0x01)
+		{
+			increaseTimer++;
+			state = WaitInc;
+			break;
+		}
+		else if (tempA == 0x03)
+		{
+			state = Reset;
+			break;
+		}
+		else
+		{
+			state = Begin;
+			break;
+		}
+	case WaitDec:
+		if (decreaseTimer >= 10)
+		{
+			state = Decrease;
+			break;
+		}
+		else if (tempA == 0x02)
+		{
+			decreaseTimer++;
+			state = WaitDec;
+			break;
+		}
+		else if (tempA == 0x03)
+		{
+			state = Reset;
+			break;
+		}
+		else
+		{
+			state = Begin;
+			break;
+		}
+	case Reset:
+		if ((tempA == 0x01) || (tempA == 0x02))
+		{
+			state = Reset; break;
+		}
+		else
+		{
+			state = Begin; break;
+		}
+	default:
+		state = Start;
+		break;
+	}
+
+	switch (state) { //State actions
+	case Begin:
+		break;
+	case Increase:
+	{
+		if (tempB >= 0x09)
+		{
+			tempB = 0x09;
+			break;
+		}
+		else
+		{
+			tempB = tempB + 0x01;
+			break;
+		}
+	}
+	case Decrease:
+	{
+		if (tempB <= 0x00)
+		{
+			tempB = 0x00;
+			break;
+		}
+		else
+		{
+			tempB = tempB - 0x01;
+			break;
+		}
+	}
+	case Reset:
+	{
+		tempB = 0x00;
+		break;
+	}
+	default:
+		break;
+	}
+
+	LCD_Cursor(1);
+	LCD_WriteData(tempB + '0');
+}
 
 int main(void)
 {
-	DDRB = 0xFF;
+	state = Start;
 	tempB = 0x00;
-	state = START;
-	TimerSet(1000);
+	DDRA = 0x00;	PORTA = 0xFF;
+	DDRC = 0xFF;	PORTC = 0x00;
+	DDRD = 0xFF;	PORTD = 0x00;
+	TimerSet(100);
 	TimerOn();
-	while (1) {
+	LCD_init();
+	LCD_WriteData(tempB + '0');
+	while (1)
+	{
 		Tick();
-		// Wait 1 sec
 		while (!TimerFlag);
 		TimerFlag = 0;
 	}
-}
-
-void Tick() {
-	switch (state) { //transitions
-	case START:
-	{
-		tempB = 0x00;
-		state = FIRST;
-		break;
-	}
-	case FIRST:
-	{
-		state = SECOND;
-		break;
-	}
-	case SECOND:
-	{
-		state = THIRD;
-		break;
-	}
-	case THIRD:
-	{
-		state = FIRST;
-		break;
-	}
-	}
-	switch (state) { //state actions
-	case START:
-		break;
-
-	case FIRST:
-	{
-		tempB = 0x01;
-		break;
-	}
-	case SECOND:
-	{
-		tempB = 0x02;
-		break;
-	}
-	case THIRD:
-	{
-		tempB = 0x04;
-		break;
-	}
-	}
-	PORTB = tempB;
+	return 0;
 }
